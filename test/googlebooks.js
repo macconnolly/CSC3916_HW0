ccef8bea462e5a0e2bd2445b997cbb544269fa72
Mac Connolly <mac@macconnolly.com>
2019-02-06 19:11:10 -0700

    Added PM

--- test/googlebooks.js
+++ test/googlebooks.js
@@ -12,9 +12,8 @@ describe('Test Async Books', () => {
     it('should return an object with list books with turing in the title', function(done) {
         getBooks('turing').then((result) => {
             let fmtObj = JSON.parse(result);
-
             let books = fmtObj.data;
-
+            console.log(JSON.stringify(books))
             expect(books).to.be.an('object');
             expect(books.items).to.satisfy(function(items) {
                 return items.every(function(item) {
